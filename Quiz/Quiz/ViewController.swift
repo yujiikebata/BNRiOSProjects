2d712063d7a631231fdd8eb0bb11ab790021fac8
yujiikebata <yujiikebata@gmail.com>
2015-12-26 21:43:35 +0900

    create and implement action methods

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -12,12 +12,29 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    let questions: [String] = ["From what is cognac made?",
+                                "What is 7+7",
+                                "What is the capital of Vermont?"]
+    let answers: [String] = ["Grapes",
+                            "14",
+                            "Montpelier"]
+    var currentQuestionIndex: Int = 0
+    
     @IBAction func showNextQuestion(sender: AnyObject) {
+        ++currentQuestionIndex
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
         
     }
     
     @IBAction func showAnswer(sender: AnyObject) {
-        
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
     }
 }
 
