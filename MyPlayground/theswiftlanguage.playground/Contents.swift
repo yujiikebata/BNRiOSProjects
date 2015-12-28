5c0fb780731408ab8602eb8f659d7606f8580f9f
yujiikebata <yujiikebata@gmail.com>
2015-12-28 18:16:10 +0900

    use optional (and force unwrapping and optional binding)

--- MyPlayground/theswiftlanguage.playground/Contents.swift
+++ MyPlayground/theswiftlanguage.playground/Contents.swift
@@ -39,3 +39,23 @@ emptyString.isEmpty
 
 // Instance methods
 countingUp.append("three")
+
+// Optionals
+var anOptionalFloat: Float?
+var anOptionalArrayOfStrings: [String]?
+var anOptionalArrayOfOptionalStrings: [String?]?
+
+var reading1: Float?
+var reading2: Float?
+var reading3: Float?
+
+reading1 = 9.8
+reading2 = 9.2
+//reading3 = 9.7
+if let r1 = reading1,
+    r2 = reading2,
+    r3 = reading3 {
+        let avgReading = (r1 + r2 + r3) / 3
+} else {
+    let errorString = "Instrument reported a reading that was nil."
+}
