e862abd130f5f9f6189556a72a8b1b8349639dbc
yujiikebata <yujiikebata@gmail.com>
2015-12-28 14:12:06 +0900

    use literal assignment and subscripting

--- MyPlayground/theswiftlanguage.playground/Contents.swift
+++ MyPlayground/theswiftlanguage.playground/Contents.swift
@@ -6,11 +6,16 @@ var str = "Hello, playground"
 str = "Hello, Swift"
 let constStr = str
 
+let number = 42
+let fmStation = 91.1
+
+let countingUp = ["one", "two"]
+let secondElement = countingUp[1]
+let nameByParkingSpace = [13: "Alice", 27: "Bob"]
+
 var nextYear: Int
 var bodyTemp: Float
 var hasPet: Bool
 var arrayOfInts: Array<Int>
-var arrayOfInts: [Int]
 var dictionaryOfCapitalsByCountry: Dictionary<String, String>
-var dictionaryOfCapitalsByCountry: [String, String]
 var winningLotteryNumbers: Set<Int>
