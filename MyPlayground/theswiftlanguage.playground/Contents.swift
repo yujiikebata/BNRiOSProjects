11177f8ae22564cd67fbb49b308c6b739d8b7d68
yujiikebata <yujiikebata@gmail.com>
2015-12-28 14:22:40 +0900

    use initializers

--- MyPlayground/theswiftlanguage.playground/Contents.swift
+++ MyPlayground/theswiftlanguage.playground/Contents.swift
@@ -2,20 +2,33 @@
 
 import UIKit
 
+// just wrote something...
 var str = "Hello, playground"
 str = "Hello, Swift"
 let constStr = str
-
-let number = 42
-let fmStation = 91.1
-
 let countingUp = ["one", "two"]
 let secondElement = countingUp[1]
 let nameByParkingSpace = [13: "Alice", 27: "Bob"]
 
+// Types
 var nextYear: Int
 var bodyTemp: Float
 var hasPet: Bool
 var arrayOfInts: Array<Int>
 var dictionaryOfCapitalsByCountry: Dictionary<String, String>
 var winningLotteryNumbers: Set<Int>
+
+// Initializer
+let emptyString = String()
+let emptyArrayOfInts = [Int]()
+let emptySetOfFloats = Set<Float>()
+let defaultNumber = Int()
+let defaultBool = Bool()
+let number = 42
+let meaningOfLife = String(number)
+let availableRooms = Set([205, 411, 412])
+let defaultFloat = Float()
+let flaotFromLiteral = Float(3.14)
+let easyPi = 3.14
+let floatFromDouble = Float(easyPi)
+let floatingPi: Float = 3.14
