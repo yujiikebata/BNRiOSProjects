95f9a6464cc66fb83f37a50359d55054aac6d994
yujiikebata <yujiikebata@gmail.com>
2015-12-29 18:41:04 +0900

    create repository WorldTrotter

---
+++ WroldTrotter/WroldTrotter/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  WroldTrotter
+//
+//  Created by yujiikebata on 12/29/15.
+//  Copyright © 2015 yujiikebata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+}
+
