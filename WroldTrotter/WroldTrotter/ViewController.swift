997a58dbdf4222ea3c67f7b2af97ed7cdd51d4d1
yujiikebata <yujiikebata@gmail.com>
2015-12-29 18:41:41 +0900

    override viewDidLoad and create subview

--- WroldTrotter/WroldTrotter/ViewController.swift
+++ WroldTrotter/WroldTrotter/ViewController.swift
@@ -9,5 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blueColor()
+        view.addSubview(firstView)
+    }
 }
 
