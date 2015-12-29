c5f91520af52b03968a04141805ebcbb5ac1a49f
yujiikebata <yujiikebata@gmail.com>
2015-12-29 19:10:36 +0900

    customize the labels

--- WroldTrotter/WroldTrotter/ViewController.swift
+++ WroldTrotter/WroldTrotter/ViewController.swift
@@ -9,13 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.blueColor()
-        view.addSubview(firstView)
-    }
+
 }
 
