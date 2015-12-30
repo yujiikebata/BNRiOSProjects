426d01c3fc8be82b6637a23cb199e268cc3b8c18
yujiikebata <yujiikebata@gmail.com>
2015-12-31 07:24:24 +0900

    replace default ViewController with ConversionViewController

---
+++ WroldTrotter/WroldTrotter/ConversionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ConversionViewController.swift
+//  WroldTrotter
+//
+//  Created by yujiikebata on 12/31/15.
+//  Copyright © 2015 yujiikebata. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CoversionViewController: UIViewController {
+    
+}
