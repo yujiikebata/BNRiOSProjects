d7eb9d206c0047b882bd897aff89ef2948eed021
yujiikebata <yujiikebata@gmail.com>
2016-01-01 20:07:14 +0900

    fixed typo (project name)

--- worldTrotter/WroldTrotter/ConversionViewController.swift
+++ worldTrotter/WroldTrotter/ConversionViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class CoversionViewController: UIViewController {
+class CoversionViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
