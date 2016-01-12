a48313926e4efed858e2208435062662efedbf79
yujiikebata <yujiikebata@gmail.com>
2016-01-12 19:56:48 +0900

    create ItemViewController

---
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by yujiikebata on 1/12/16.
+//  Copyright © 2016 yujiikebata. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
