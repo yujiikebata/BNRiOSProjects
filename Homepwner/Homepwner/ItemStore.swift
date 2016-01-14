c8baa94b9ed0cf4aa92213a21d1331e30847974d
yujiikebata <yujiikebata@gmail.com>
2016-01-14 20:25:19 +0900

    create ItemStore and add createItem method

---
+++ Homepwner/Homepwner/ItemStore.swift
@@ -0,0 +1,22 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by yujiikebata on 1/14/16.
+//  Copyright © 2016 yujiikebata. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+}
