5058215d8f9e31adc061cfce0f3472f385db8265
yujiikebata <yujiikebata@gmail.com>
2016-01-16 17:36:27 +0900

    set Content Insets for preventing contents from overlapping status bar

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -12,6 +12,18 @@ import UIKit
 class ItemsViewController: UITableViewController {
     var itemStore: ItemStore!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Get the height of the status bar
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
     }
