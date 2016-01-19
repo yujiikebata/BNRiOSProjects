599db06af88becbddc026a908940cd3f9b637669
yujiikebata <yujiikebata@gmail.com>
2016-01-19 20:28:54 +0900

    remove initializer in itemStore

--- Homepwner/Homepwner/ItemStore.swift
+++ Homepwner/Homepwner/ItemStore.swift
@@ -12,12 +12,6 @@ import UIKit
 class ItemStore {
     var allItems = [Item]()
     
-    init() {
-        for _ in 0..<5 {
-            createItem()
-        }
-    }
-    
     func createItem() -> Item {
         let newItem = Item(random: true)
         
