8dbdbfd0b6c00b26472248676dd6d058e8d28d31
yujiikebata <yujiikebata@gmail.com>
2016-01-25 20:30:28 +0900

    create ItemCell layout

---
+++ Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by yujiikebata on 1/25/16.
+//  Copyright © 2016 yujiikebata. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+}
