bf66c262934feb101a590a7d9f1644b56f51a4c1
yujiikebata <yujiikebata@gmail.com>
2016-02-04 22:09:27 +0900

    create DetailViewController

---
+++ Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by yujiikebata on 2/4/16.
+//  Copyright © 2016 yujiikebata. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
