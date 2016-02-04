716f802751bd4581cb062b47ed02347cf32d9e89
yujiikebata <yujiikebata@gmail.com>
2016-02-04 22:15:02 +0900

    add Outlets and connect to storyboard

--- Homepwner/Homepwner/DetailViewController.swift
+++ Homepwner/Homepwner/DetailViewController.swift
@@ -11,4 +11,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
 }
