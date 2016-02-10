9eccd5ce4fa3d9a4477ea918502befeb9431a0ab
yujiikebata <yujiikebata@gmail.com>
2016-02-10 22:20:53 +0900

    set item in viewWillAppear

--- Homepwner/Homepwner/DetailViewController.swift
+++ Homepwner/Homepwner/DetailViewController.swift
@@ -15,4 +15,15 @@ class DetailViewController: UIViewController {
     @IBOutlet var serialNumberField: UITextField!
     @IBOutlet var valueField: UITextField!
     @IBOutlet var dateLabel: UILabel!
+    
+    var item: Item!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+        valueField.text = "\(item.valueInDollars)"
+        dateLabel.text = "\(item.dateCreated)"
+    }
 }
